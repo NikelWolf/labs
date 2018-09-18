d2c54c3f5a5c7fdc09eb095e9306c59ed9e1130b
Nik <snive2013@yandex.ru>
2018-09-18 02:19:17 +0000

    practice works 1-7 added

---
+++ term_3/oop/practice/src/ru/mirea/sidnev/practice/practice1/task2/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.sidnev.practice.practice1.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Book b = new Book("Java Cookbook", "Ian F. Darwin", 864);
+        System.out.println(b);
+    }
+}
---
+++ term_3/oop/practice/src/ru/mirea/sidnev/practice/practice2/task1/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.sidnev.practice.practice2.task1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Rob Pike", "", 'm');
+        System.out.println(a);
+    }
+}
